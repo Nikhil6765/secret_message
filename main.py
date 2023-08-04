6207d102568f016f3b2261ab39b57032ed1fddc9
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-08-05 00:38:55 +0530

    add

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ def get_message():
     message=simpledialog.askstring('Message','Enter the secret message: ')
     return message
 
+root=Tk()
+
+while True:
+    task=get_task()
+    if task=='encrypt':
+        message=get_message()
+        messagebox.showinfo('Message to encrypt is: ',message)
+    
+    elif task=='decrypt':
+        message=get_message()
+        messagebox.showinfo('Message to decrypt is: ',message)
+
+    else:
+        break
