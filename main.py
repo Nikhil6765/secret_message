466b46443925b86d60443ba35a05a86662b7701b
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-08-05 00:34:31 +0530

    done

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from tkinter import messagebox,simpledialog,Tk
 def get_task():
     task=simpledialog.askstring('Task', 'Do you want to encrypt or decrypt')
     return task
+
+
+def get_message():
+    message=
